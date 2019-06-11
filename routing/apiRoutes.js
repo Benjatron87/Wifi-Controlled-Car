849c99c3dbda2cc90441038c6b5c2761cc821577
Benjatron <benjt122@gmail.com>
2019-06-11 14:22:57 -0400

    got rid of the get in api routes

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -5,11 +5,8 @@ module.exports = function(app) {
     app.post("/direction", (req, res)=> {
         console.log(req.body);
 
-        direction = req.body;        
+        direction = req.body;  
+        
+        res.json(direction);
     })
-
-    app.get("/direction", (req, res)=> {
-
-      res.json(direction);      
-  })
 };
